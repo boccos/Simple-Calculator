b56d2d9034e7b16cec11114e767125c58f1489d6
boccos <145885073+boccos@users.noreply.github.com>
2025-10-02 22:24:19 -0400

    Functional Calculator

--- calculator.js
+++ calculator.js
@@ -34,36 +34,104 @@ buttons.forEach(button => {
     }
 
     else{
+      if (value == "C"){
+        var1 = null;
+        var2 = null;
+        operator = null;
+        updateDisplay("");
+        resultDisplayed = false;
+        operatorPressed = false;
+      }
+
       if (["+", "-", "x", "÷"].includes(value)){
+        if (resultDisplayed) {
+          operator = value;
+          operatorPressed = true;
+          resultDisplayed = false;
+          currentInput = "";
+          return;
+        }
+
+        if (operator != null && currentInput != ""){
+          var2 = currentInput;
+
+          if (operator == "+"){
+            var1 = add(+var1, +var2);
+          }
+
+          if (operator == "-"){
+            var1 = subtract(+var1, +var2);
+          }
+
+          if (operator == "x"){
+            var1 = multiply(+var1, +var2);
+          }
+
+          if (operator == "÷"){
+            var1 = divide(+var1, +var2);
+
+            if (var1 === "Error") {
+              updateDisplay(var1);
+              var1 = null;
+              var2 = null;
+              operator = null;
+              currentInput = "";
+              resultDisplayed = false;
+              operatorPressed = false;
+              return;
+            }
+          }
+
+          updateDisplay(var1);
+          currentInput = "";
+          operator = value;
+          operatorPressed = true;
+          resultDisplayed = false;
+        }
+
+        else if (var1 == null && currentInput != "") {
         var1 = +currentInput;
         operator = value;
         operatorPressed = true;
+        }
       }
-
       if (value == "="){
-        var2 = currentInput;
-        currentInput = "";
-        if (operator == "+"){
-          updateDisplay(add(+var1, +var2));
-          resultDisplayed = true;
-        }
+        if (operator != null && currentInput != ""){
+          var2 = currentInput;
+          currentInput = "";
+          if (operator == "+"){
+            var1 = add(+var1, +var2);
+            updateDisplay(var1);
+            resultDisplayed = true;
+          }
+          if (operator == "-"){
+            var1 = subtract(+var1, +var2);
+            updateDisplay(var1);
+            resultDisplayed = true;
+          }
+          if (operator == "x"){
+            var1 = multiply(+var1, +var2);
+            updateDisplay(var1);
+            resultDisplayed = true;
+          }
+          if (operator == "÷"){
+            var1 = divide(+var1, +var2);
+            updateDisplay(var1);
+            resultDisplayed = true;
 
-        if (operator == "-"){
-          updateDisplay(subtract(+var1, +var2));
-          resultDisplayed = true;
+            if (var1 === "Error") {
+              updateDisplay(var1);
+              var1 = null;
+              var2 = null;
+              operator = null;
+              currentInput = "";
+              resultDisplayed = false;
+              operatorPressed = false;
+              return;
+            }
+          }
+          operator = null;
         }
-
-        if (operator == "x"){
-          updateDisplay(multiply(+var1, +var2));
-          resultDisplayed = true;
-        }
-
-        if (operator == "÷"){
-          updateDisplay(divide(+var1, +var2));
-          resultDisplayed = true;
-        }
-
-        operator = null;
       }
     }
   })
@@ -87,5 +155,8 @@ const multiply = function(num1, num2) {
 }
 
 const divide = function(num1, num2) {
-    return num1 / num2;
+  if (num2 == 0){
+    return "Error";
+  }
+  return num1 / num2;
 }
